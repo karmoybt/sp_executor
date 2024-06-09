cc4b5676546ef7c84faa804a9d22f102bc8ea796
karmoy <javimartin@vivaldi.net>
2024-06-09 11:29:17 +0200

    Funciona , pero revisar los parametros de cuando lo pasa

--- src/routes/stored_procedure.rs
+++ src/routes/stored_procedure.rs
@@ -1,10 +1,12 @@
 use warp::Filter;
-use std::sync::Arc;
-use rbatis::rbatis::RBatis;
-use serde_json::Value as JsonValue; // Alias para el tipo de serde_json::Value
-use rbs::Value as RbsValue; // Alias para el tipo de rbs::Value
+use serde_json::Value as JsonValue;
 use crate::models::response::Response;
+use std::sync::Arc;
 use std::convert::Infallible;
+use indexmap::IndexMap;
+use rbs::value::map::ValueMap;
+use rbs::Value as RbsValue;
+use rbatis::rbatis::RBatis;
 
 pub fn sp_route(rb: Arc<RBatis>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("SP" / String)
@@ -15,23 +17,45 @@ pub fn sp_route(rb: Arc<RBatis>) -> impl Filter<Extract = impl warp::Reply, Erro
 }
 
 async fn call_stored_procedure(sp_name: String, params: JsonValue, rb: Arc<RBatis>) -> Result<impl warp::Reply, Infallible> {
-    let sql = format!("EXEC {}", sp_name);
+    let mut sql = format!("EXEC {}", sp_name);
+    let mut rbs_params = vec![];
 
-    // Convertir params de serde_json::Value a rbs::Value
-    let rbs_params: RbsValue = match serde_json::from_value(params) {
-        Ok(value) => value,
-        Err(e) => {
-            eprintln!("Failed to convert params to rbs::Value: {:?}", e);
+    println!("Sp nombre: {}", sp_name);
+    println!("Parámetros: {:?}", params);
+
+    match params {
+        JsonValue::Object(ref map) => {
+            for (k, v) in map.iter() {
+                let k_str = k.to_string();  // Directly convert key to string
+                let param = match json_to_rbs_value(&v) {
+                    Ok(val) => val,
+                    Err(e) => {
+                        eprintln!("Failed to convert param {} to rbs::Value: {:?}", k_str, e);
+                        return Ok(warp::reply::json(&Response {
+                            data: RbsValue::Null.into(),
+                            status: "error".to_string(),
+                            message: format!("Error: {:?}", e),
+                        }));
+                    }
+                };
+                if !sql.is_empty() {
+                    sql.push_str(", ");
+                }
+                sql.push_str(&format!("@{}", k_str));
+                rbs_params.push(param);
+            }
+        }
+        _ => {
+            eprintln!("Invalid parameters: {:?}", params);
             return Ok(warp::reply::json(&Response {
                 data: RbsValue::Null.into(),
                 status: "error".to_string(),
-                message: format!("Error: {:?}", e),
+                message: "Invalid parameters".to_string(),
             }));
         }
-    };
+    }
 
-    // Ejecutar la consulta y manejar los errores
-    match rb.query(&sql, vec![rbs_params]).await {
+    match rb.query(&sql, rbs_params).await {
         Ok(rows) => {
             Ok(warp::reply::json(&Response {
                 data: rows.into(),
@@ -46,6 +70,37 @@ async fn call_stored_procedure(sp_name: String, params: JsonValue, rb: Arc<RBati
                 status: "error".to_string(),
                 message: format!("Error: {:?}", e),
             }))
+        }
+    }
+}
+
+fn json_to_rbs_value(value: &JsonValue) -> Result<RbsValue, String> {
+    match value {
+        JsonValue::Null => Ok(RbsValue::Null),
+        JsonValue::Bool(b) => Ok(RbsValue::Bool(*b)),
+        JsonValue::Number(num) => {
+            if let Some(i) = num.as_i64() {
+                Ok(RbsValue::I64(i))
+            } else if let Some(u) = num.as_u64() {
+                Ok(RbsValue::U64(u))
+            } else if let Some(f) = num.as_f64() {
+                Ok(RbsValue::F64(f))
+            } else {
+                Err("Unsupported number type".to_string())
+            }
+        }
+        JsonValue::String(s) => Ok(RbsValue::String(s.clone())),
+        JsonValue::Array(arr) => {
+            let vec = arr.iter().map(json_to_rbs_value).collect::<Result<Vec<_>, _>>()?;
+            Ok(RbsValue::Array(vec))
+        }
+        JsonValue::Object(obj) => {
+            let mut map = IndexMap::new();
+            for (k, v) in obj.iter() {
+                let k_str = k.to_string();
+                map.insert(RbsValue::String(k_str.clone()), json_to_rbs_value(v)?);
+            }
+            Ok(RbsValue::Map(ValueMap(map)))
         }
     }
 }
